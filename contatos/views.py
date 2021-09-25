5e7c630003b7d9898ce00ba296434310cd3ec64f
JeanAfonso <jean_bro@hotmail.com>
2021-09-24 23:02:05 -0300

    Adicionando filter e order by

--- contatos/views.py
+++ contatos/views.py
@@ -6,7 +6,7 @@ from .models import Contato
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(mostrar=True)
     paginator = Paginator(contatos, 5)
 
     page = request.GET.get('p')
@@ -17,4 +17,8 @@ def index(request):
 
 def ver_contato(request, contato_id):
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404
+
     return render(request, "contatos/ver_contato.html", {"contato": contato})
